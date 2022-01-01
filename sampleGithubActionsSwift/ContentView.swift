f11edb48886145961f302438110b2789d9e899a8
king-kazu39 <magmanakazato@gmail.com>
2022-01-01 16:36:12 +0900

    first commit

---
+++ sampleGithubActionsSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sampleGithubActionsSwift
+//
+//  Created by kazuya on 2022/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sampleGithubActionsSwift/sampleGithubActionsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleGithubActionsSwiftApp.swift
+//  sampleGithubActionsSwift
+//
+//  Created by kazuya on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct sampleGithubActionsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
