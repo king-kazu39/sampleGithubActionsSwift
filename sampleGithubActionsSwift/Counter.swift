ea1638554eeec0032dd920cfa7f8b31ac9795123
Kazuya Nakazato <magmanakazato@gmail.com>
2022-01-01 22:31:17 +0900

    テストコード作成に伴いyamlファイル修正 (#2)
    * テストコード作成に伴いyamlファイル修正
    
    * commit漏れ

---
+++ sampleGithubActionsSwift/Counter.swift
@@ -0,0 +1,24 @@
+//
+//  Counter.swift
+//  sampleGithubActionsSwift
+//
+//  Created by kazuya on 2022/01/01.
+//
+
+import Foundation
+
+struct Counter {
+    var count: Int = 0
+    
+    mutating func plus() {
+        count += 1
+    }
+    
+    mutating func minus() {
+        count -= 1
+    }
+    
+    mutating func reset() {
+        count = 0
+    }
+}
