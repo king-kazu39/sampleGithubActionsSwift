43fa81c6a4c47ad1948ef09141df04ea97bd6819
king-kazu39 <magmanakazato@gmail.com>
2022-01-02 01:49:42 +0900

    SwiftLintをSPMで入れるのをやめた。微修正

--- sampleGithubActionsSwift/Counter.swift
+++ sampleGithubActionsSwift/Counter.swift
@@ -10,15 +10,9 @@ import Foundation
 struct Counter {
     var count: Int = 0
 
-    mutating func plus(){
-        count += 1
-    }
+    mutating func plus() { count += 1 }
 
-    mutating func minus(){
-        count -= 1
-    }
+    mutating func minus() { count -= 1}
 
-    mutating func reset(){
-        count = 0
-    }
+    mutating func reset() { count = 0 }
 }
--- sampleGithubActionsSwiftUITests/sampleGithubActionsSwiftUITestsLaunchTests.swift
+++ sampleGithubActionsSwiftUITests/sampleGithubActionsSwiftUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class SampleGithubActionsSwiftUITestsLaunchTests: XCTestCase {
+class SampleGithubActionsUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
